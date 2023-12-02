bfafc28dc511986b8067836abc908e5fede50605
Gilberto <gilbertogjn@gmail.com>
2023-12-01 22:11:00 -0400

    inicial

---
+++ exercicio_ts/exercicio_ts.js
@@ -0,0 +1,9 @@
+"use strict";
+function multiplicacao(a, b) {
+    return a * b;
+}
+console.log(multiplicacao(2, 4));
+function saudacao(nome) {
+    return 'Olá ' + nome;
+}
+console.log(saudacao('Gilberto'));
---
+++ exercicio_ts/exercicio_ts.ts
@@ -0,0 +1,11 @@
+function multiplicacao(a: number, b: number): number {
+    return a * b
+}
+
+console.log(multiplicacao(2, 4))
+
+function saudacao(nome: string): string {
+    return 'Olá ' + nome
+}
+
+console.log(saudacao('Gilberto'))
